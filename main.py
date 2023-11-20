50cbbfbcae10fb774585db2fe7e1ef8437e7faa1
mleo <leo.mace@ynov.com>
2023-11-20 16:52:17 +0100

    Map first step

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from ursina import *
 from direct.actor.Actor import Actor
+import map
 
 app = Ursina()
+
+map.initMap()
+camera.position = (5, 5, -20)
 
 app.run()
---
+++ map.py
@@ -0,0 +1,30 @@
+from ursina import *
+
+UNITE_DE_MESURE = 1 * 0.5
+
+
+#     0 -> Air    1 -> Sol    2 -> Mur 3 -> Sol Volant
+
+mapTab = [
+    [2,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,0,0,2],
+    [2,3,0,0,0,0,3,0,0,2],
+    [2,0,0,0,0,0,0,0,0,2],
+    [2,1,1,0,0,0,0,0,0,2],
+    [2,0,0,0,0,0,0,3,0,2],
+    [2,0,0,0,0,0,0,0,0,2],
+    [2,1,1,1,1,1,1,1,1,2]
+]
+
+def initMap():
+    for y, ligne in enumerate(mapTab):
+        for x, valeur in enumerate(ligne):
+            if(valeur == 2):
+                Entity(model='quad', color=color.red, scale=(1, 1), position=(x, len(mapTab) - y))
+            if(valeur == 1):
+                Entity(model='quad', color=color.green, scale=(1, 1), position=(x, len(mapTab) - y), texture='Assets/floor.png', collider='block')
+            if(valeur == 3):
+                Entity(model='quad', color=color.green, scale=(1, 0.65), position=(x, len(mapTab) - y), texture='Assets/pxArt.png', collider='block')
+
