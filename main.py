a8a492994aca83e5acb7e7cd7de7c201b53c9a3c
mleo <leo.mace@ynov.com>
2023-11-20 16:19:17 +0100

    Clean main

--- main.py
+++ main.py
@@ -3,11 +3,4 @@ from direct.actor.Actor import Actor
 
 app = Ursina()
 
-entity = Entity()
-#animations are stored within the file
-actor = Actor("filename.gltf")
-actor.reparent_to(entity)
-
-actor.loop("animation_name")  # use .play() instead of loop() to play it once.
-
 app.run()
