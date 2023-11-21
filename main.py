be542f8eaabbf7c9d0527b4346a540f5e80cda59
mathisDeronne <mathis.deronne@gmail.com>
2023-11-21 14:29:49 +0100

    pull

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from ursina import *
 from direct.actor.Actor import Actor
 
+def on_play_button_click():
+    print("Bouton PLAY cliqué !")
+
 app = Ursina()
-
-entity = Entity()
-#animations are stored within the file
-actor = Actor("filename.gltf")
-actor.reparent_to(entity)
-
-actor.loop("animation_name")  # use .play() instead of loop() to play it once.
+text = Text(text="WELCOME TO DUCK.EXE", scale=4, origin=(0,-2))
+play_button = Button(text='PLAY', color=color.green, scale=(0.2, 0.1), origin=(0,2), on_click=on_play_button_click)
 
 app.run()
+
