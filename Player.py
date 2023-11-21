94fba74255858d37593ae31a5c1bf80aa72f8fc2
lorensviguie <112950422+lorensviguie@users.noreply.github.com>
2023-11-21 16:40:39 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ Player.py
@@ -0,0 +1,81 @@
+from ursina import *
+from character import Character,Dice
+
+class Player(Character):
+    _move_left = ''
+    _move_right = ''
+    _jump = ''
+    _attack = ''
+    __texture = ''
+
+    def get_texture(self):
+        return str(self.__texture)
+
+    def __init__(self,name='test',max_hp= 10,attack=2,defense=3,dice = Dice(6), texture_creation='guerrier', move_left_choice='a', move_right_choice='d', jump_choice='space',attack_choice = 'w'):
+        super().__init__(name=name, max_hp=max_hp, attack=attack, defense=defense, dice=dice, texture_creation=texture_creation)
+        self._move_left = move_left_choice
+        self._move_right = move_right_choice
+        self._jump = jump_choice
+        self._attack = attack_choice
+        self.__texture = texture_creation
+        self.gravity = 5
+        self.jump_height = 1
+        self.jump_speed = 4
+        self.y_speed = 0
+        self.is_jumping = False
+        self.is_attacking = False
+
+
+    def update(self):
+        if self.is_attacking:
+            temp = str('./asset/'+ self.get_texture().split('.')[0] +'_attack.png')
+                     
+            
+            self.texture = temp
+        self.y_speed -= self.gravity * time.dt
+        self.y += self.y_speed * time.dt
+        # Gestion du déplacement avec les touches du clavier
+        if held_keys[self._move_left]:
+            self.x -= 5 * time.dt
+        if held_keys[self._move_right]:
+            self.x += 5 * time.dt
+
+        # Gestion des collisions avec la carte
+        hit_info = raycast(self.position, self.down, distance=0.4, ignore=[self])
+        if hit_info.hit:
+            self.y = hit_info.world_point[1] + 0.4  # Ajustement de la hauteur après la collision
+            self.y_speed = 0
+            self.is_jumping = False
+
+        # Gestion du saut avec la barre d'espace
+        if held_keys[self._jump] and not self.is_jumping:
+            self.y_speed = self.jump_speed
+            self.is_jumping = True
+
+        # Gestion de l'attaque avec la touche spécifiée
+        if held_keys[self._attack]:
+            self.attack_animation()
+
+        # Rétablir le skin original après la fin de l'attaque
+        if not self.is_attacking:
+            self.texture =str('./asset/'+ str(self.get_texture()))
+
+    def attack_animation(self):
+        self.is_attacking = True
+        invoke(self.end_attack_animation, delay=0.2)  # Appeler end_attack_animation après 0.5 secondes
+
+    def end_attack_animation(self):
+        self.is_attacking = False
+
+
+player2 = Player(
+    name="Guerrier",
+    max_hp=10,
+    attack=2,
+    defense=3,
+    texture_creation="guerrier",
+    move_left_choice='j',
+    move_right_choice='l',
+    jump_choice='k'
+)
+print(player2.get_texture())
--- main.py
+++ main.py
@@ -1,13 +1,62 @@
 from ursina import *
-from direct.actor.Actor import Actor
+from Player import Player
+from character import Dice
+
+class GameMap(Entity):
+    def __init__(self, size=20):
+        super().__init__()
+        self.size = size
+        self.create_map()
+
+    def create_map(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                cube = Entity(
+                    model='cube',
+                    texture='grass_texture.png',
+                    collider='box',
+                    scale=(1, 0.1, 1),
+                    position=(i - self.size // 2, 0, j - self.size // 2)
+                )
+class HealthBar:
+    def __init__(self, player: Player):
+        self.player = player
+        self.border = Entity(parent=camera.ui, model='quad', z=1, y=-0.4, x=0.4, color=color.black, scale_x=player._max_hp * 0.05, scale_y=0.02)
+        self.health = Entity(parent=camera.ui, model='quad', z=1, y=-0.4, x=0.4, color=color.red, scale_x=player._current_hp * 0.05, scale_y=0.015)
+        self.name_text = Text(parent=camera.ui, text=player._name, z=1, y=-0.45, x=0.4, scale=0.05)
+
+    def update(self):
+        self.health.scale_x = self.player._current_hp / self.player._max_hp * self.border.scale_x
+
 
 app = Ursina()
 
-entity = Entity()
-#animations are stored within the file
-actor = Actor("filename.gltf")
-actor.reparent_to(entity)
+# Création de la carte
+game_map = GameMap(size=20)
 
-actor.loop("animation_name")  # use .play() instead of loop() to play it once.
+# Création du joueur
+
+player2 = Player(
+    name="Guerrier",
+    max_hp=10,
+    attack=2,
+    defense=3,
+    texture_creation="guerrier",
+    move_left_choice='j',
+    move_right_choice='l',
+    jump_choice='k'
+)
+player1 = Player(
+    name="frog",
+    max_hp=10,
+    attack=2,
+    defense=3,
+    texture_creation="frog",
+    move_left_choice='a',
+    move_right_choice='d',
+    jump_choice='space'
+)
+health_bar = HealthBar(player=player1)
+
 
 app.run()
